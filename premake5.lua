537aefbc6213db96fb673b1afe8e6d9e262ff866
Matthew J. Berger <matthewberger@nevada.unr.edu>
2016-09-18 22:18:40 -0700

    Update premake file with a function to generate more demo projects easily.

--- premake5.lua
+++ premake5.lua
@@ -1,44 +1,52 @@
 -- premake5.lua
-workspace "OpenGL"
-    configurations { "Debug", "Release" }
-    location "build"
+function generate()
+    workspace "OpenGL"
+        configurations { "Debug", "Release" }
+        location "build"
 
-local name = "triangle"
-project(name)
-    local target = "bin/" .. name .. "/%{cfg.buildcfg}"
-    targetdir(target)
-    kind "WindowedApp"
-    language "C++"
-    links{ "SDL2", "SDL2main", "SDL2_image", "SDL2_ttf", "SDL2_mixer" }
-    includedirs { "core", "core/**", "glad/include/" }
-    files({ "core/**.cpp", name .. "/**.h", name .. "/**.cpp", "glad/src/glad.c" })
+    create_example "triangle"
+end
 
-    filter "configurations:Debug"
-        defines "DEBUG"
-        flags "Symbols"
+-- Demo project creation function
+function create_example(name)
+    project(name)
+        local target = "bin/" .. name .. "/%{cfg.buildcfg}"
+        targetdir(target)
+        kind "WindowedApp"
+        language "C++"
+        links{ "SDL2", "SDL2main", "SDL2_image", "SDL2_ttf", "SDL2_mixer" }
+        includedirs { "core", "core/**", "glad/include/" }
+        files({ "core/**.cpp", name .. "/**.h", name .. "/**.cpp", "glad/src/glad.c" })
 
-    filter "configurations:Release"
-        defines "NDEBUG"
-        optimize "On"
+        filter "configurations:Debug"
+            defines "DEBUG"
+            flags "Symbols"
 
-    configuration { "linux", "gmake" }
-        links{ "GL", "dl" } -- libdl is required by glad on non-windows platforms
-        buildoptions { "`sdl2-config --cflags`", "-std=c++11" }
-        linkoptions "`sdl2-config --libs`"
-        -- Copy shaders to the output directory
-        postbuildcommands("cp -r ../" .. name .."/shaders ../bin/" .. name .. "/%{cfg.buildcfg}")
+        filter "configurations:Release"
+            defines "NDEBUG"
+            optimize "On"
 
-	configuration { "windows" }
-		links "OpenGL32.lib" -- link OpenGL on windows
-		local sdl2_path = os.getenv("SDL2_PATH")
-		local sdl2_include = (sdl2_path or "") .. "/include"
-		local sdl2_lib = (sdl2_path or "") .. "/lib/x86"
-		local glm_path = os.getenv("GLM_PATH")
-		debugdir(target)
-		includedirs { sdl2_include, glm_path }
-		libdirs { sdl2_lib }
-		-- Post build events in Visual Studio to copy shaders to the output directory
-		postbuildcommands({
-		"xcopy /E /Y /I \"$(SolutionDir)../" .. name .. "/shaders\" \"$(TargetDir)\"",
-		"xcopy /E /Y /I \"" .. (string.gsub(sdl2_lib, "/", "\\") or "") .. "\\*.dll" .. "\" \"$(TargetDir)\"" })
+        configuration { "linux", "gmake" }
+            links{ "GL", "dl" } -- libdl is required by glad on non-windows platforms
+            buildoptions { "`sdl2-config --cflags`", "-std=c++11" }
+            linkoptions "`sdl2-config --libs`"
+            -- Copy shaders to the output directory
+            postbuildcommands("cp -r ../" .. name .."/shaders ../bin/" .. name .. "/%{cfg.buildcfg}")
 
+        configuration { "windows" }
+            links "OpenGL32.lib" -- link OpenGL on windows
+            local sdl2_path = os.getenv("SDL2_PATH")
+            local sdl2_include = (sdl2_path or "") .. "/include"
+            local sdl2_lib = (sdl2_path or "") .. "/lib/x86"
+            local glm_path = os.getenv("GLM_PATH")
+            debugdir(target)
+            includedirs { sdl2_include, glm_path }
+            libdirs { sdl2_lib }
+            -- Post build events in Visual Studio to copy shaders to the output directory
+            postbuildcommands({
+            "xcopy /E /Y /I \"$(SolutionDir)../" .. name .. "/shaders\" \"$(TargetDir)\"",
+            "xcopy /E /Y /I \"" .. (string.gsub(sdl2_lib, "/", "\\") or "") .. "\\*.dll" .. "\" \"$(TargetDir)\"" })
+end
+
+-- Generate build files
+generate()
